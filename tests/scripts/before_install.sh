0ce8e39932dfbd7b7606cb0c92e7e99547b80816
Sergei Antipov <sergei@gravitational.com>
2020-11-08 22:39:55 +0700

    Disable mitogen

--- tests/scripts/before_install.sh
+++ tests/scripts/before_install.sh
@@ -12,13 +12,11 @@ set -o errexit
 set -o pipefail
 
 # Latest Ansible install
-pip install docker ansible mitogen
+pip install docker ansible
 
 cat << EOF > ansible.cfg
 [defaults]
 pipelining = True
-strategy = mitogen_linear
-strategy_plugins = /home/travis/virtualenv/python2.7.15/lib/python2.7/site-packages/ansible_mitogen/plugins/strategy
 EOF
 
 # Pull docker image or build it
